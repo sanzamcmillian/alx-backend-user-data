0bf47a57f486cc28a1b7d2aa9924f600219cacb7
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-01 22:24:38 +0200

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Method to encrypt a password"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks if encrypted password is valid"""
+    
