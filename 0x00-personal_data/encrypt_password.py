4cbc3b3b22dfc49a758776b3bd7efd7171f3dea2
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-01 22:09:17 +0200

    hashpw

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 """Encrypting Passwords"""
+import bcrypt
 
 
-def hash_password
+def hash_password(password: str) -> bytes:
+    """Method to encrypt a password"""
+    password = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password, salt)
