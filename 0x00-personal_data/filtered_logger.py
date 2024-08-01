4bc63558deaa2e30361b2af230406dcaff1ca7d9
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-01 11:04:31 +0200

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+""""""
+import re
+from typing import List
+
+
+patterns = {
+    'extract': lambda x, y: r"(?P<field>{})=[^{}]*".format("|".join(x), y),
+    'replace': lambda x: r"\g<field>={}".format(x),
+}
+
+
+def filter_datum(fields, redaction, message, separator):
+    """Function to filter data using Regex"""
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
