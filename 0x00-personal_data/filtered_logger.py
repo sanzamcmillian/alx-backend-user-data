7bd06f92ee5eeb9e5204f74f67a8d66a952f2147
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-01 14:58:56 +0200

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,7 @@ patterns = {
     'extract': lambda x, y: r"(?P<field>{})=[^{}]*".format("|".join(x), y),
     'replace': lambda x: r"\g<field>={}".format(x),
 }
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -16,6 +17,17 @@ def filter_datum(fields: List[str], redaction: str,
     """Function to filter data using Regex"""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+def get_logger() -> logging.Logger:
+    """Function to create a logger"""
+    logger = logging.getLogger("user_data")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger.addHandler(stream_handler)
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
