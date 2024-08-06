4ac54fa416b942e04b1aa635cc8c4c04b9a2ba39
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-06 11:53:30 +0200

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,6 +26,7 @@ def unauthorized(error) -> str:
     """Unauthorized error handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """forbidden error handler"""
