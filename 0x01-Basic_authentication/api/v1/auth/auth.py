5d897ceba32d5292ffa7e523e491eb1899ca09a2
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-06 22:08:31 +0200

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,13 +2,25 @@
 """manage the API authentication"""
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
     """class to manage API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """method 1"""
-        return False
+        """path for authentication"""
+        if path is not None and excluded_paths is not None:
+            for i in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if i[-1] == '*':
+                    pattern = '{}.*'.format(i[0:-1])
+                elif i[-1] == '/':
+                    pattern = '{}*'.format(i[0:-1])
+                else:
+                    pattern = '{}.*'.format(i)
+                if re.match(pattern, path):
+                    return False
+            return True
 
     def authorization_header(self, request=None) -> str:
         """method 2"""
