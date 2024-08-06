29646e573ca867af57784250146029f76f7b36c2
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-06 12:04:33 +0200

    pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,11 +9,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """method 1"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """method 2"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """method 3"""
         return None
