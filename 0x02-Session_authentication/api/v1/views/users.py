92c2ae83232ba101affd41afe1b28c08253b9caf
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-13 09:31:12 +0200

    fix

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -28,7 +28,7 @@ def view_one_user(user_id: str = None) -> str:
     if user_id is None:
         abort(404)
     if user_id == 'me':
-        if request.currrent_user is None:
+        if request.current_user is None:
             abort(404)
         else:
             return jsonify(request.current_user.to_json())
