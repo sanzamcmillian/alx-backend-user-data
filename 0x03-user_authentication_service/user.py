52b462c2a8861c8f964c6f0c00bf6f28aca3233e
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-14 10:45:18 +0200

    task 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 """module to create a user SQLAlchemy"""
-from SQLAlchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
 
 
 class User(Base):
     """class to implement User"""
     __tablename__ = 'User'
-    
+
     id = Column(Integer, primary_key=True)
-    email = Column(String)
-    hashed_password = Column(String)
-    session_id = Column(String)
-    reset_token = Column(String)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
