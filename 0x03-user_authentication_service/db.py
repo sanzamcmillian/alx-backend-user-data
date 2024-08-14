c5c46571ce0e2dc7066636ea5053e7606001269b
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-14 11:24:34 +0200

    try 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,11 +32,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """method to add a user to db"""
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            new_user = None
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
         return new_user
