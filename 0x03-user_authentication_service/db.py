dbbf90720c4d8016b2d0aeaf658ef77bb7dc3055
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-14 11:56:56 +0200

    commit

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,4 +34,5 @@ class DB:
         """method to add a user to db"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
+        self._session.commit()
         return new_user
